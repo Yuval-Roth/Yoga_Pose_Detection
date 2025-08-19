1c1332f23dd22cdf91cab1218e71d8d0e5e96f92
Yuval Roth <yuvalroth123@gmail.com>
2025-08-19 15:40:42 +0300

    detecting multiple people works

--- pose_detection/mediapipe_pose.py
+++ pose_detection/mediapipe_pose.py
@@ -1,52 +1,82 @@
 import cv2
 import mediapipe as mp
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
 from mediapipe.tasks import python
-from body_parts.body_parts import *
+from mediapipe.tasks.python import vision
+import numpy as np
 
-# Initialize MediaPipe Pose
-mp_pose = mp.solutions.pose
-pose = mp_pose.Pose(model_complexity=2)
-mp_drawing = mp.solutions.drawing_utils
 
-# Open webcam
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 960)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 540)
-cap.set(cv2.CAP_PROP_FPS, 60)
+def draw_landmarks_on_image(rgb_image, detection_result):
+  pose_landmarks_list = detection_result.pose_landmarks
+  annotated_image = np.copy(rgb_image)
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        break
+  # Loop through the detected poses to visualize.
+  for idx in range(len(pose_landmarks_list)):
+    pose_landmarks = pose_landmarks_list[idx]
 
-    # Convert the frame to RGB (MediaPipe expects RGB)
-    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    # Draw the pose landmarks.
+    pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+    pose_landmarks_proto.landmark.extend([
+      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
+    ])
+    solutions.drawing_utils.draw_landmarks(
+      annotated_image,
+      pose_landmarks_proto,
+      solutions.pose.POSE_CONNECTIONS,
+      solutions.drawing_styles.get_default_pose_landmarks_style())
+  return annotated_image
 
-    # Process the frame with MediaPipe Pose
-    results = pose.process(rgb_frame)
+while True:
+    cap = cv2.VideoCapture(0)  # 0 = default webcam
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+    # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    cap.set(cv2.CAP_PROP_FPS, 30)
 
-    # Draw the landmarks on the frame
-    if results.pose_landmarks:
+    if not cap.isOpened():
+        print("Error: Could not open camera.")
+        exit(1)
 
-        body_parts = build_body_parts(results.pose_landmarks)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Failed to grab frame")
+            exit(1)
 
-        torso = body_parts[BodyParts.TORSO]
-        cv2.putText(frame, f"Torso vector: {torso.vector}", (10, 30),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), 1)
-
-        mp_drawing.draw_landmarks(
-            frame,
-            results.pose_landmarks,
-            mp_pose.POSE_CONNECTIONS,
-            mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2),
-            mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=2),
+        # STEP 2: Create an PoseLandmarker object.
+        base_options = python.BaseOptions(model_asset_path='model/pose_landmarker_heavy.task')
+        options = vision.PoseLandmarkerOptions(
+            base_options=base_options,
+            output_segmentation_masks=True,
+            num_poses=5,
         )
+        detector = vision.PoseLandmarker.create_from_options(options)
 
-    # Show the output
-    cv2.imshow("MediaPipe Pose", frame)
+        # Convert OpenCV frame (BGR → RGB)
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    if cv2.waitKey(1) & 0xFF == 27:  # Press ESC to quit
-        break
+        # Convert numpy array → MediaPipe Image
+        image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
 
-cap.release()
-cv2.destroyAllWindows()
+        # STEP 4: Detect pose landmarks from the input image.
+        detection_result = detector.detect(image)
+
+        # STEP 5: Process the detection result. In this case, visualize it.
+        annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
+        cv2.imshow("Mediapipe Pose", cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
+
+        # segmentation_mask = detection_result.segmentation_masks[0].numpy_view()
+        # visualized_mask = np.repeat(segmentation_mask[:, :, np.newaxis], 3, axis=2) * 255
+        # cv2.imshow("2", visualized_mask)
+
+        # cv2.imshow("Camera Feed", frame)
+
+        # Press 'q' to quit
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
